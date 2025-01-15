2e99a5cc6e6ab78690c9cc6d4b1770fdba7db996
Jevin Sweval <jevinsweval@gmail.com>
2025-01-15 00:27:05 -0500

    wip

--- bindings-oneshot.c
+++ bindings-oneshot.c
@@ -24,21 +24,41 @@ static void my_parser_example(char const *const fileName, char const *const file
     size_t f2_sz = 0;
     char *f2     = open_file(fileName2, &f2_sz);
 
-    assert(Z3_open_log("bindings-oneshot-log.log"));
+    // assert(Z3_open_log("bindings-oneshot-log.log"));
     Z3_config cfg;
     Z3_context ctx;
     cfg = Z3_mk_config();
     // Z3_set_param_value(cfg, "model", "true");
-    Z3_set_param_value(cfg, "trace", "true");
+    // Z3_set_param_value(cfg, "trace", "true");
     ctx = Z3_mk_context(cfg);
     Z3_del_config(cfg);
 
-    printf("f1:\n%s\n", Z3_eval_smtlib2_string(ctx, f1));
-    // printf("f1 Z3_solver_check: %u\n", Z3_)
-    printf("f2:\n%s\n", Z3_eval_smtlib2_string(ctx, f2));
+    Z3_solver solver = Z3_mk_solver(ctx);
+    // Z3_solver_inc_ref(ctx, solver);
+    // printf("solver init: %s\n", Z3_solver_to_string(ctx, solver));
+
+    // printf("f1:\n%s\n", Z3_eval_smtlib2_string(ctx, f1));
+    // Z3_solver_eval_smtlib2_string
+    Z3_solver_from_string(ctx, solver, f1);
+    // printf("f1 solver pre-check: %s\n", Z3_solver_to_string(ctx, solver));
+    printf("f1 Z3_solver_check: %d\n", Z3_solver_check(ctx, solver));
+    // printf("f1 solver post-check: %s\n", Z3_solver_to_string(ctx, solver));
+
+    Z3_solver_reset(ctx, solver);
+    // printf("solver post-reset: %s\n", Z3_solver_to_string(ctx, solver));
+    // Z3_solver_dec_ref(ctx, solver);
+    Z3_solver solver2 = solver;
+    // Z3_solver solver2 = Z3_mk_solver(ctx);
+    // printf("solver init2: %s\n", Z3_solver_to_string(ctx, solver2));
+
+    // printf("f2:\n%s\n", Z3_eval_smtlib2_string(ctx, f2));
+    // Z3_solver_from_string(ctx, solver2, f2);
+    // printf("f2 solver pre-check: %s\n", Z3_solver_to_string(ctx, solver2));
+    printf("f2 Z3_solver_check: %d\n", Z3_solver_check(ctx, solver2));
+    // printf("f2 solver post-check: %s\n", Z3_solver_to_string(ctx, solver2));
 
     Z3_del_context(ctx);
-    Z3_close_log();
+    // Z3_close_log();
 
     free(f2);
     free(f1);
--- smt2-samples/sat-foo.smt2
+++ smt2-samples/sat-foo.smt2
@@ -12,5 +12,5 @@
 (declare-const v1 Int)
 
 (assert (and (and (and (and (and (and (and (not (= (+ (+ 8 (- 56 v3)) v5) 64)) (and (<= 0 (- v3 1)) (< (- v3 1) 64))) (and (<= 0 (- (- 56 v3) 1)) (< (- (- 56 v3) 1) 64))) v4) (not v0)) (not (and v0 (>= v3 55)))) (= v3 v2)) (= v1 (ite v0 55 63))))
-(check-sat)
+; (check-sat)
 ; (pop)
