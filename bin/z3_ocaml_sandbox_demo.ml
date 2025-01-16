483f9aad9642cfb0c7aea3bfc458b5e6ac840b15
Jevin Sweval <jevinsweval@gmail.com>
2025-01-16 16:54:02 -0500

    nit

--- bin/z3_ocaml_sandbox_demo.ml
+++ bin/z3_ocaml_sandbox_demo.ml
@@ -1,10 +1,1 @@
-open Z3
-
-let () = print_endline (Z3_ocaml_sandbox.hello ());
-let () = print_endline (Z3_ocaml_sandbox.hello ());
-
-
-let cfg = [("model", "true");] in
-let ctx = (mk_context cfg) in
-let foo = Format.printf "lol\n" in
-foo ctx
+let () = print_endline (Z3_ocaml_sandbox.hello ())
