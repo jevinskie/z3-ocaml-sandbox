24e9c797bbef0144e18badc32f7280d16b3c4258
Jevin Sweval <jevinsweval@gmail.com>
2025-01-17 20:54:26 -0500

    wip

--- z3_mini/z3_mini.c
+++ z3_mini/z3_mini.c
@@ -6,18 +6,18 @@
 
 #include <z3.h>
 
-struct z3_mini_ctx_impl {
+struct Z3_mini_ctx_impl {
     Z3_context ctx;
     Z3_solver solver;
 };
 
-z3_mini_ctx Z3_mini_init_context(z3_mini_ctx ctx) {
+Z3_mini_ctx Z3_mini_init_context(Z3_mini_ctx ctx, int model) {
     assert(ctx);
     Z3_config cfg = Z3_mk_config();
     if (!cfg) {
         return NULL;
     }
-    Z3_set_param_value(cfg, "model", "false");
+    Z3_set_param_value(cfg, "model", model ? "true" : "false");
     ctx->ctx = Z3_mk_context(cfg);
     Z3_del_config(cfg);
     if (!ctx->ctx) {
@@ -31,28 +31,47 @@ z3_mini_ctx Z3_mini_init_context(z3_mini_ctx ctx) {
     return ctx;
 }
 
-z3_mini_ctx Z3_mini_mk_context(void) {
-    z3_mini_ctx res = malloc(sizeof(*res));
+Z3_mini_ctx Z3_mini_mk_context(int model) {
+    Z3_mini_ctx res = malloc(sizeof(*res));
     if (!res) {
         return NULL;
     }
-    return Z3_mini_init_context(res);
+    return Z3_mini_init_context(res, model);
 }
 
-void Z3_mini_deinit_context(z3_mini_ctx ctx) {
+void Z3_mini_deinit_context(Z3_mini_ctx ctx) {
     assert(ctx);
     Z3_del_context(ctx->ctx);
 }
 
-void Z3_mini_del_context(z3_mini_ctx ctx) {
+void Z3_mini_del_context(Z3_mini_ctx ctx) {
     assert(ctx);
     Z3_mini_deinit_context(ctx);
     free(ctx);
 }
 
-int Z3_mini_check_sat(z3_mini_ctx ctx, const char *smt2) {
+int Z3_mini_check_sat(Z3_mini_ctx ctx, const char *smt2) {
     assert(ctx && smt2);
     Z3_solver_reset(ctx->ctx, ctx->solver);
     Z3_solver_from_string(ctx->ctx, ctx->solver, smt2);
     return Z3_solver_check(ctx->ctx, ctx->solver);
+}
+
+char *Z3_mini_check_sat_get_model(Z3_mini_ctx ctx, const char *smt2) {
+    assert(ctx && smt2);
+    Z3_solver_reset(ctx->ctx, ctx->solver);
+    Z3_solver_from_string(ctx->ctx, ctx->solver, smt2);
+    int sat = Z3_solver_check(ctx->ctx, ctx->solver);
+    if (sat == 1) {
+        Z3_model model = Z3_solver_get_model(ctx->ctx, ctx->solver);
+        assert(model);
+        const char *model_str = Z3_model_to_string(ctx->ctx, model);
+        assert(model_str);
+        return strdup(model_str);
+    }
+    return NULL;
+}
+
+void Z3_mini_del_string(char *s) {
+    free(s);
 }
--- z3_mini/z3_mini.h
+++ z3_mini/z3_mini.h
@@ -3,14 +3,16 @@
 
 HEDLEY_BEGIN_C_DECLS
 
-struct z3_mini_ctx_impl;
+struct Z3_mini_ctx_impl;
 
-typedef struct z3_mini_ctx_impl *z3_mini_ctx;
+typedef struct Z3_mini_ctx_impl *Z3_mini_ctx;
 
-extern HEDLEY_PUBLIC z3_mini_ctx Z3_mini_init_context(z3_mini_ctx ctx);
-extern HEDLEY_PUBLIC z3_mini_ctx Z3_mini_mk_context(void);
-extern HEDLEY_PUBLIC void Z3_mini_deinit_context(z3_mini_ctx ctx);
-extern HEDLEY_PUBLIC void Z3_mini_del_context(z3_mini_ctx ctx);
-extern HEDLEY_PUBLIC int Z3_mini_check_sat(z3_mini_ctx ctx, const char *smt2);
+extern HEDLEY_PUBLIC Z3_mini_ctx Z3_mini_init_context(Z3_mini_ctx ctx, int model);
+extern HEDLEY_PUBLIC Z3_mini_ctx Z3_mini_mk_context(int model);
+extern HEDLEY_PUBLIC void Z3_mini_deinit_context(Z3_mini_ctx ctx);
+extern HEDLEY_PUBLIC void Z3_mini_del_context(Z3_mini_ctx ctx);
+extern HEDLEY_PUBLIC int Z3_mini_check_sat(Z3_mini_ctx ctx, const char *smt2);
+extern HEDLEY_PUBLIC char *Z3_mini_check_sat_get_model(Z3_mini_ctx ctx, const char *smt2);
+extern HEDLEY_PUBLIC void Z3_mini_del_string(char *s);
 
 HEDLEY_END_C_DECLS
