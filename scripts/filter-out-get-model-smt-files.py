21bfdc3f7417fd70e469dc7143a3be556793e3fc
Jevin Sweval <jevinsweval@gmail.com>
2025-01-22 02:39:56 -0500

    script update

--- scripts/filter-out-get-model-smt-files.py
+++ scripts/filter-out-get-model-smt-files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 
 from path import Path
 
@@ -11,31 +11,44 @@ def do_want(p: Path) -> bool:
     return p.suffix == ".smt2"
 
 
-src = Path(sys.argv[1])
-dst = Path(sys.argv[2])
+def main(in_path: Path, out_path: Path, push_pop: bool = False, check_sat: bool = False) -> None:
+    for f in Path(in_path).walkfiles(do_want):
+        with open(f) as inf:
+            smt = inf.read()
+            inf.close()
+            if "(get-model)" in smt:
+                continue
+            lines = []
+            for ln in smt.splitlines():
+                if ln.startswith(";"):
+                    continue
+                if ln == "":
+                    continue
+                if ln == "(push)" and not push_pop:
+                    continue
+                if ln == "(pop)" and not push_pop:
+                    continue
+                if ln == "(check-sat)" and not check_sat:
+                    continue
+                lines.append(ln)
+            tf = "\n".join(lines)
+            relp = in_path.relpathto(f)
+            dp = out_path / relp
+            dp.parent.makedirs_p()
+            with open(dp, "w") as of:
+                of.write(tf)
 
-for f in Path(src).walkfiles(do_want):
-    with open(f, "r") as inf:
-        smt = inf.read()
-        inf.close()
-        if "(get-model)" in smt:
-            continue
-        lines = []
-        for l in smt.splitlines():
-            if l.startswith(";"):
-                continue
-            if l == "":
-                continue
-            if l == "(push)":
-                continue
-            if l == "(pop)":
-                continue
-            if l == "(check-sat)":
-                continue
-            lines.append(l)
-        tf = "\n".join(lines)
-        relp = src.relpathto(f)
-        dp = dst / relp
-        dp.parent.makedirs_p()
-        with open(dp, "w") as of:
-            of.write(tf)
+
+def get_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--push-pop", action="store_true", help="Add push/pop statements")
+    parser.add_argument("-c", "--check-sat", action="store_true", help="Add check-sat statement")
+    parser.add_argument("in_dir", type=Path, help="Input directory")
+    parser.add_argument("out_dir", type=Path, help="Output directory")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    main(args.in_dir, args.out_dir, push_pop=args.push_pop, check_sat=args.check_sat)
