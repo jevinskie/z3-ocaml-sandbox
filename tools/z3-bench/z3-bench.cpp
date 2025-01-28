2244c7f6604ced010e1b2e10e25da5f0afbad8a4
Jevin Sweval <jevinsweval@gmail.com>
2025-01-27 23:10:23 -0500

    wip

--- tools/z3-bench/z3-bench.cpp
+++ tools/z3-bench/z3-bench.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <cstring>
 #include <filesystem>
+#include <limits>
 #include <mutex>
 #include <pthread/qos.h>
 #include <span>
@@ -12,14 +13,18 @@
 #include <sys/mman.h>
 #include <sys/qos.h>
 #include <sys/stat.h>
+#include <type_traits>
 #include <unistd.h>
 #include <vector>
 
-namespace fs = std::filesystem;
-
 #include <BS_thread_pool.hpp>
 #include <fmt/format.h>
+#include <sha2/sha2.hpp>
 #include <z3.h>
+
+#define INLINE [[gnu::always_inline]]
+
+namespace fs = std::filesystem;
 
 typedef enum Z3_mini_lbool {
     Z3_MINI_L_FALSE = -1,
@@ -27,6 +32,174 @@ typedef enum Z3_mini_lbool {
     Z3_MINI_L_TRUE,
     Z3_MINI_L_UNINIT,
 } Z3_mini_lbool;
+
+template <typename T>
+    requires(std::unsigned_integral<T>)
+class AtomicWaiter {
+public:
+    AtomicWaiter(T count) : m_cnt{count} {};
+    void release(T num = 1) {
+        m_cnt -= num;
+        m_cnt.notify_all();
+    }
+    void wait() {
+        T cur = m_cnt;
+        while (cur) {
+            m_cnt.wait(cur);
+            cur = m_cnt;
+        }
+    }
+
+private:
+    std::atomic<T> m_cnt;
+};
+
+template <BS::opt_t OT = BS::none> class ThreadPool : public BS::thread_pool<OT> {
+public:
+    /**
+     * @brief A helper class to divide a range into blocks. Used by `detach_blocks()`, `submit_blocks()`,
+     * `detach_loop()`, and `submit_loop()`.
+     *
+     * @tparam T The type of the indices. Should be a signed or unsigned integer.
+     */
+    template <typename T> class [[nodiscard]] blocks {
+    public:
+        /**
+         * @brief Construct a `blocks` object with the given specifications.
+         *
+         * @param first_index_ The first index in the range.
+         * @param index_after_last_ The index after the last index in the range.
+         * @param num_blocks_ The desired number of blocks to divide the range into.
+         */
+        blocks(const T first_index_, const T index_after_last_, const std::size_t num_blocks_) noexcept
+            : first_index(first_index_), index_after_last(index_after_last_), num_blocks(num_blocks_) {
+            if (index_after_last > first_index) {
+                const std::size_t total_size = static_cast<std::size_t>(index_after_last - first_index);
+                num_blocks                   = std::min(num_blocks, total_size);
+                block_size                   = total_size / num_blocks;
+                remainder                    = total_size % num_blocks;
+                if (block_size == 0) {
+                    block_size = 1;
+                    num_blocks = (total_size > 1) ? total_size : 1;
+                }
+            } else {
+                num_blocks = 0;
+            }
+        }
+
+        /**
+         * @brief Get the index after the last index of a block.
+         *
+         * @param block The block number.
+         * @return The index after the last index.
+         */
+        [[nodiscard]] T end(const std::size_t block) const noexcept {
+            return (block == num_blocks - 1) ? index_after_last : start(block + 1);
+        }
+
+        /**
+         * @brief Get the number of blocks. Note that this may be different than the desired number of blocks that was
+         * passed to the constructor.
+         *
+         * @return The number of blocks.
+         */
+        [[nodiscard]] std::size_t get_num_blocks() const noexcept {
+            return num_blocks;
+        }
+
+        /**
+         * @brief Get the first index of a block.
+         *
+         * @param block The block number.
+         * @return The first index.
+         */
+        [[nodiscard]] T start(const std::size_t block) const noexcept {
+            return first_index + static_cast<T>(block * block_size) +
+                   static_cast<T>(block < remainder ? block : remainder);
+        }
+
+    private:
+        /**
+         * @brief The size of each block (except possibly the last block).
+         */
+        std::size_t block_size = 0;
+
+        /**
+         * @brief The first index in the range.
+         */
+        T first_index = 0;
+
+        /**
+         * @brief The index after the last index in the range.
+         */
+        T index_after_last = 0;
+
+        /**
+         * @brief The number of blocks.
+         */
+        std::size_t num_blocks = 0;
+
+        /**
+         * @brief The remainder obtained after dividing the total size by the number of blocks.
+         */
+        std::size_t remainder = 0;
+    }; // class blocks
+
+    INLINE void wait_on_n_tasks(size_t n, const auto &task) {
+        AtomicWaiter waiter(n);
+        for (size_t i = 0; i < n; ++i) {
+            push_task([&, i] {
+                task(i);
+                waiter.release();
+            });
+        }
+        waiter.wait();
+    }
+
+    template <typename F, typename Idx1, typename Idx2, typename Idx = std::common_type_t<Idx1, Idx2>>
+    // requires(F(size_t{}, Idx{}, Idx{}))
+    std::enable_if_t<std::is_invocable_v<F, size_t, Idx, Idx>>
+    parallelize_indexed_loop(const Idx1 first_index, const Idx2 index_after_last, const F &loop,
+                             const size_t num_blocks = 0) {
+        blocks blks(first_index, index_after_last, num_blocks ? num_blocks : ThreadPool<OT>::get_thread_count());
+        const auto num_blks = blks.get_num_blocks();
+        AtomicWaiter waiter(num_blks);
+        for (size_t i = 0; i < num_blks; ++i) {
+            detach_task([&, i] {
+                loop(i, blks.start(i), blks.end(i));
+                waiter.release();
+            });
+        }
+        waiter.wait();
+    }
+
+    template <typename F, typename Idx>
+    // requires(F(size_t{}, Idx{}, Idx{}))
+    std::enable_if_t<std::is_invocable_v<F, size_t, Idx, Idx>>
+    parallelize_indexed_loop(const Idx index_after_last, const F &loop, const size_t num_blocks = 0) {
+        parallelize_indexed_loop(Idx{0}, index_after_last, loop, num_blocks);
+    }
+};
+
+static char nibble_to_ascii_hex(const uint8_t chr) {
+    if (chr <= 9) {
+        return chr + '0';
+    } else if (chr >= 0xA && chr <= 0xF) {
+        return chr - 0xA + 'a';
+    } else {
+        std::abort();
+    }
+}
+
+static std::string to_string(const sha2::sha256_hash &h) {
+    std::string ds;
+    ds.resize(2 * 256 / 8);
+    for (size_t i = 0; i < sizeof(h); ++i) {
+        ds[2 * i]     = nibble_to_ascii_hex((h[i] >> 4) & 0xF);
+        ds[2 * i + 1] = nibble_to_ascii_hex(h[i] & 0xF);
+    }
+    return ds;
+}
 
 static std::vector<char> slurp_file(const fs::path &path) {
     const auto fd = ::open(path.c_str(), O_RDONLY);
