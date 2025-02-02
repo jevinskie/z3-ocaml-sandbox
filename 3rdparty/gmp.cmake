4c9b89610b349883ce5fb2e574ed5f6f3e5cd7aa
Jevin Sweval <jevinsweval@gmail.com>
2025-02-02 18:29:33 -0500

    broken wip

--- 3rdparty/gmp.cmake
+++ 3rdparty/gmp.cmake
@@ -1,11 +1,10 @@
-file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gmp-build/tmp")
+#file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gmp-build/tmp")
 
 ExternalProject_Add(gmp_ext
     SOURCE_DIR          ${CMAKE_CURRENT_SOURCE_DIR}/gmp
-    CONFIGURE_COMMAND   cd ${CMAKE_CURRENT_BINARY_DIR}/gmp-build/tmp &&
-                        rsync -a ${CMAKE_CURRENT_SOURCE_DIR}/gmp/ . &&
-                        ./.bootstrap &&
-                        ./configure
+    CONFIGURE_COMMAND   cd <SOURCE_DIR> &&
+                        ./.bootstrap
+    BUILD_COMMAND       <SOURCE_DIR>/configure
                         CC=${CMAKE_C_COMPILER}
                         CFLAGS=${CMAKE_C_FLAGS}
                         CXX=${CMAKE_CXX_COMPILER}
@@ -13,87 +12,26 @@ ExternalProject_Add(gmp_ext
                         LDFLAGS=${CMAKE_MODULE_LINKER_FLAGS}
                         AR=${CMAKE_AR}
                         RANLIB=${CMAKE_RANLIB}
-                        --prefix=${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix
+                        --prefix=<INSTALL_DIR>
                         ${CONFIGURE_SHARED}
                         ${CONFIGURE_STATIC}
-                        --disable-cxx
-    BUILD_COMMAND       cd ${CMAKE_CURRENT_BINARY_DIR}/gmp-build/tmp &&
-                        make -j ${NCPUS} install MAKEINFO=true
-    INSTALL_COMMAND     ""
+                        --disable-cxx &&
+                        make -j ${NCPUS} MAKEINFO=true
+    INSTALL_COMMAND     make -j ${NCPUS} install MAKEINFO=true
     PREFIX              gmp-build
     INSTALL_DIR         gmp-prefix
-    # BUILD_JOB_SERVER_AWARE TRUE
+    BUILD_JOB_SERVER_AWARE TRUE
     # BUILD_IN_SOURCE     OFF
-    BUILD_BYPRODUCTS    gmp-prefix/include/gmp.h gmp-prefix/lib/libgmp.a
-    STEP_TARGETS build
+    INSTALL_BYPRODUCTS    ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include/gmp.h ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a
+    # STEP_TARGETS build
+    STEP_TARGETS install
     EXCLUDE_FROM_ALL ON
 )
 
 file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
 
-
-# if (BUILD_SHARED_LIBS)
-#     add_library(GMP INTERFACE EXCLUDE_FROM_ALL)
-# else()
-#     add_library(GMP INTERFACE EXCLUDE_FROM_ALL)
-# endif()
-
-# set_target_properties(GMP PROPERTIES EXCLUDE_FROM_ALL OFF)
-# target_link_libraries(GMP INTERFACE gmp-prefix/lib/libgmp${LIB_EXT})
-# target_include_directories(GMP INTERFACE gmp-prefix/include)
-# target_include_directories(GMP INTERFACE
-# $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include/gmp>
-# $<INSTALL_INTERFACE:include/gmp>
-# )
-
-# install(TARGETS GMP
-    # EXPORT libz3
-#     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-#     ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-#     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-#     INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-# )
-
-# install(IMPORTED_RUNTIME_ARTIFACTS GMP
-    # EXPORT GMPExport
-    # LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    # ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    # RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-    # INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-# )
-
-
-# add_library(GMP::GMP UNKNOWN IMPORTED)
-# target_include_directories(GMP INTERFACE
-#     $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include>
-#     $<INSTALL_INTERFACE:include>
-# )
-# target_link_libraries(GMP::GMP INTERFACE
-#     $<BUILD_INTERFACE:"${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp${LIB_EXT}">
-#     $<INSTALL_INTERFACE:"lib/libgmp${LIB_EXT}">
-# )
-# set_target_properties(GMP::GMP PROPERTIES
-#     IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp${LIB_EXT}"
-#     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include"
-# #    IMPORTED_LINK_INTERFACE_LANGUAGES "C"  # Or "CXX" for C++
-# )
-
-# add_dependencies(GMP::GMP gmp_ext-complete gmp_ext gmp_ext-build ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include/gmp.h)
-# add_library(GMP ALIAS GMP::GMP)
-
-# set(GMP_FOUND ON)
-# set(GMP_C_INCLUDES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
-# set(GMP_CXX_INCLUDES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
-# set(GMP_INCLUDE_DIRS "${GMP_C_INCLUDES}" "${GMP_CXX_INCLUDES}")
-# set(GMP_C_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a")
-# set(GMP_CXX_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmpxx.a")
-# list(REMOVE_DUPLICATES GMP_INCLUDE_DIRS)
-# uset_target_properties(GMP::GMP PROPERTIES
-#     INTERFACE_INCLUDE_DIRECTORIES "${GMP_C_INCLUDES}"
-#         IMPORTED_LOCATION "${GMP_C_LIBRARIES}")
-
 add_library(GMP::GMP UNKNOWN IMPORTED)
-add_dependencies(GMP::GMP gmp_ext)
+add_dependencies(GMP::GMP gmp_ext-install)
 
 set_target_properties(GMP::GMP PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include"
