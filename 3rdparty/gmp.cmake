56407f3f2da1833a79f77a95d762e9484e685a3e
Jevin Sweval <jevinsweval@gmail.com>
2025-02-01 23:23:24 -0500

    wip

--- 3rdparty/gmp.cmake
+++ 3rdparty/gmp.cmake
@@ -3,7 +3,7 @@
 ExternalProject_Add(gmp_ext
     SOURCE_DIR          ${CMAKE_CURRENT_SOURCE_DIR}/gmp
     CONFIGURE_COMMAND   rsync -a ${CMAKE_CURRENT_SOURCE_DIR}/gmp/ . &&
-                        autoreconf -fiv &&
+                        ./.bootstrap &&
                         ./configure
                         CC=${CMAKE_C_COMPILER}
                         CFLAGS=${CMAKE_C_FLAGS}
@@ -15,18 +15,19 @@ ExternalProject_Add(gmp_ext
                         --prefix=${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix
                         ${CONFIGURE_SHARED}
                         ${CONFIGURE_STATIC}
-    # BUILD_COMMAND       make -j ${NCPUS} MAKEINFO=true
-    INSTALL_COMMAND     make -j ${NCPUS} install MAKEINFO=true
+                        --disable-cxx
     BUILD_COMMAND       make -j ${NCPUS} install MAKEINFO=true
-    # INSTALL_COMMAND     true
+    INSTALL_COMMAND     ""
     PREFIX              gmp-build
     INSTALL_DIR         gmp-prefix
     # BUILD_JOB_SERVER_AWARE TRUE
     # BUILD_IN_SOURCE     OFF
-    BUILD_BYPRODUCTS    gmp-prefix/lib/libgmp.a gmp-prefix/lib/libgmpxx.a gmp-prefix/include/gmp.h gmp-prefix/include/gmpxx.h
-    # (Adjust for parallel builds, host/target options, etc. as needed)
+    BUILD_BYPRODUCTS    gmp-prefix/include/gmp.h gmp-prefix/lib/libgmp.a
     # EXCLUDE_FROM_ALL
 )
+
+file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
+
 
 # if (BUILD_SHARED_LIBS)
 #     add_library(GMP INTERFACE EXCLUDE_FROM_ALL)
@@ -59,7 +60,7 @@ ExternalProject_Add(gmp_ext
 # )
 
 
-add_library(GMP::GMP UNKNOWN IMPORTED)
+# add_library(GMP::GMP UNKNOWN IMPORTED)
 # target_include_directories(GMP INTERFACE
 #     $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include>
 #     $<INSTALL_INTERFACE:include>
@@ -74,19 +75,35 @@ add_library(GMP::GMP UNKNOWN IMPORTED)
 # #    IMPORTED_LINK_INTERFACE_LANGUAGES "C"  # Or "CXX" for C++
 # )
 
-add_dependencies(GMP::GMP gmp_ext_build ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include/gmp.h)
+# add_dependencies(GMP::GMP gmp_ext-complete gmp_ext gmp_ext-build ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a ${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include/gmp.h)
+# add_library(GMP ALIAS GMP::GMP)
+
+# set(GMP_FOUND ON)
+# set(GMP_C_INCLUDES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
+# set(GMP_CXX_INCLUDES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
+# set(GMP_INCLUDE_DIRS "${GMP_C_INCLUDES}" "${GMP_CXX_INCLUDES}")
+# set(GMP_C_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a")
+# set(GMP_CXX_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmpxx.a")
+# list(REMOVE_DUPLICATES GMP_INCLUDE_DIRS)
+# uset_target_properties(GMP::GMP PROPERTIES
+#     INTERFACE_INCLUDE_DIRECTORIES "${GMP_C_INCLUDES}"
+#         IMPORTED_LOCATION "${GMP_C_LIBRARIES}")
+
+add_library(GMP::GMP UNKNOWN IMPORTED)
+add_dependencies(GMP::GMP gmp_ext)
+
+set_target_properties(GMP::GMP PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include"
+    IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a"
+)
+
 add_library(GMP ALIAS GMP::GMP)
 
-set(GMP_FOUND ON)
-set(GMP_C_INCLUDES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
-set(GMP_CXX_INCLUDES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include")
-set(GMP_INCLUDE_DIRS "${GMP_C_INCLUDES}" "${GMP_CXX_INCLUDES}")
-set(GMP_C_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmp.a")
-set(GMP_CXX_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/lib/libgmpxx.a")
-list(REMOVE_DUPLICATES GMP_INCLUDE_DIRS)
-set_target_properties(GMP::GMP PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${GMP_C_INCLUDES}"
-    IMPORTED_LOCATION "${GMP_C_LIBRARIES}")
-
 get_target_property(X GMP INTERFACE_INCLUDE_DIRECTORIES)
-message(STATUS "gmp: ${X}")
+message(WARNING "gmp include: ${X}")
+get_target_property(X GMP IMPORTED_LOCATION)
+message(WARNING "gmp lib: ${X}")
+get_target_property(X GMP::GMP INTERFACE_INCLUDE_DIRECTORIES)
+message(WARNING "gmp::gmp include: ${X}")
+get_target_property(X GMP::GMP IMPORTED_LOCATION)
+message(WARNING "gmp::gmp lib: ${X}")
--- 3rdparty/z3.cmake
+++ 3rdparty/z3.cmake
@@ -3,19 +3,29 @@ set(Z3_SINGLE_THREADED ON)
 set(Z3_POLLING_TIMER OFF) # TODO: test
 set(Z3_BUILD_LIBZ3_SHARED ${BUILD_SHARED_LIBS})
 set(Z3_BUILD_EXECUTABLE OFF)
-set(Z3_COMPONENT_EXTRA_INCLUDE_DIRS "${Z3_COMPONENT_EXTRA_INCLUDE_DIRS};${GMP_INC_DIRS}")
+# set(Z3_COMPONENT_EXTRA_INCLUDE_DIRS "${Z3_COMPONENT_EXTRA_INCLUDE_DIRS};${GMP_INC_DIRS}")
 add_subdirectory(z3)
 # target_include_directories(libz3 PUBLIC "${CMAKE_CURRENT_BINARY_DIR}/z3/src/api")
-set_target_properties(libz3 PROPERTIES EXCLUDE_FROM_ALL ON)
+# set_target_properties(libz3 PROPERTIES EXCLUDE_FROM_ALL ON)
 target_link_libraries(libz3 PUBLIC GMP)
+get_target_property(libz3_sources libz3 SOURCES)
+
+foreach(obj_target IN LISTS libz3_sources)
+    if("${obj_target}" MATCHES "^\\$<TARGET_OBJECTS:(.*)>$")
+        set(target_name "${CMAKE_MATCH_1}")
+        target_link_libraries(${target_name} PUBLIC GMP)
+    endif()
+endforeach()
+# add_dependencies(libz3 gmp-prefix/include/gmp.h)
+# add_dependencies(libz3 GMP::GMP)
 # target_include_directories(libz3 PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/z3/src/api")
-target_include_directories(libz3 INTERFACE
-    $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include>
-    $<INSTALL_INTERFACE:include>
-)
-get_target_property(GMP_INC_DIRS GMP INTERFACE_INCLUDE_DIRECTORIES)
-add_custom_target(genexdebug COMMAND ${CMAKE_COMMAND} -E echo "$<TARGET_PROPERTY:GMP,INTERFACE_INCLUDE_DIRECTORIES>")
-add_custom_target(genexdebug2 COMMAND ${CMAKE_COMMAND} -E echo "$<TARGET_PROPERTY:GMP,INCLUDE_DIRECTORIES>")
+# target_include_directories(libz3 INTERFACE
+#     $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/gmp-prefix/include>
+#     $<INSTALL_INTERFACE:include>
+# )
+# get_target_property(GMP_INC_DIRS GMP INTERFACE_INCLUDE_DIRECTORIES)
+# add_custom_target(genexdebug COMMAND ${CMAKE_COMMAND} -E echo "$<TARGET_PROPERTY:GMP,INTERFACE_INCLUDE_DIRECTORIES>")
+# add_custom_target(genexdebug2 COMMAND ${CMAKE_COMMAND} -E echo "$<TARGET_PROPERTY:GMP,INCLUDE_DIRECTORIES>")
 
 # message(WARNING "HELLO: ${fooo}")
 # target_include_directories(util PUBLIC $<TARGET_PROPERTY:GMP,INTERFACE_INCLUDE_DIRECTORIES>)
