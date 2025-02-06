0c9c7ee59ab1e3c46e8033e9c25458adbe5a4d1a
Jevin Sweval <jevinsweval@gmail.com>
2025-02-05 22:46:53 -0500

    disk thing done

--- tools/z3-bench/disk-test.cpp
+++ tools/z3-bench/disk-test.cpp
@@ -5,14 +5,18 @@
 #include <atomic>
 #include <chrono>
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
 #include <filesystem>
 #include <limits>
+#include <map>
 #include <memory>
 #include <mutex>
 #include <pthread/qos.h>
 #include <span>
+#include <sys/disk.h>
 #include <sys/fcntl.h>
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/qos.h>
 #include <sys/stat.h>
@@ -23,19 +27,17 @@
 #include <BS_thread_pool.hpp>
 #include <fmt/format.h>
 #include <fmt/std.h>
-#include <folly/FBString.h>
-#include <folly/FBVector.h>
-#include <folly/small_vector.h>
+#include <indicators/block_progress_bar.hpp>
 #include <indicators/cursor_control.hpp>
 #include <indicators/indeterminate_progress_bar.hpp>
 #include <indicators/progress_bar.hpp>
 #include <indicators/termcolor.hpp>
 
-template <class T, class Allocator = std::allocator<T>> using vector_t = folly::fbvector<T, Allocator>;
-// template <class T, class Allocator = std::allocator<T>> using vector_t = std::vector<T, Allocator>;
+// template <class T, class Allocator = std::allocator<T>> using vector_t = folly::fbvector<T, Allocator>;
+template <class T, class Allocator = std::allocator<T>> using vector_t = std::vector<T, Allocator>;
 
-using string_t = folly::fbstring;
-// using string_t = std::string;
+// using string_t = folly::fbstring;
+using string_t = std::string;
 
 #define INLINE [[gnu::always_inline]]
 
@@ -196,12 +198,30 @@ public:
     }
 };
 
-static bool has_nonzero(const uint64_t *const buf, size_t cnt) {
-#pragma unroll 8
-    for (size_t i = 0; i < cnt; ++i) {
-        if (buf[i]) {
-            return true;
+using tid_t = uint64_t;
+
+static_assert(sizeof(std::thread::id) == sizeof(tid_t));
+
+static bool has_nonzero(const int fd, const size_t buf_sz, const size_t start_elem_off, size_t cnt,
+                        const vector_t<uint8_t *> &thread_bufs) {
+    // auto buf= (uint64_t *)(thread_bufs[BS::this_thread().get_index().value()]);
+    auto buf                  = (uint64_t *)thread_bufs[0];
+    const size_t end_elem_off = start_elem_off + cnt;
+    size_t cur_elem_off       = start_elem_off;
+    while (cur_elem_off < end_elem_off) {
+        const size_t foff           = cur_elem_off * sizeof(uint64_t);
+        const size_t num_bytes_left = (end_elem_off - cur_elem_off) * sizeof(uint64_t);
+        const ssize_t try_rd_sz     = std::min(buf_sz, num_bytes_left);
+        // const auto pread_res = pread(fd, buf, try_rd_sz, foff);
+        const auto pread_res = read(fd, buf, try_rd_sz);
+        assert(pread_res == try_rd_sz);
+        const auto num_elem_read = try_rd_sz / sizeof(uint64_t);
+        for (size_t i = 0; i < num_elem_read; ++i) {
+            if (buf[i]) {
+                return true;
+            }
         }
+        cur_elem_off += num_elem_read;
     }
     return false;
 }
@@ -216,41 +236,65 @@ static void set_thread_priority_10(void) {
 
 // Function to recursively search files using thread pool
 template <BS::opt_t OptFlags>
-void search_disk(const uint64_t *const mapping, size_t sz, std::atomic<size_t> &num_nonzero_gb,
-                 vector_t<uint8_t> &non_zero, std::mutex &blobs_mutex, BS::thread_pool<OptFlags> &pool) {
-    const auto num_chunks = non_zero.size();
+void search_disk(const int fd, const size_t sz, const size_t buf_sz, BS::thread_pool<OptFlags> &pool) {
+    using namespace indicators;
+    const uint64_t *const mapping = nullptr;
+    const size_t chunk_sz_bytes   = 1024 * 1024 * 1024;
+    // const size_t chunk_sz_bytes = 1024 * 1024;
 
-    indicators::IndeterminateProgressBar bar{
-        // indicators::option::BarWidth{40},
-        indicators::option::Start{"["},
-        indicators::option::Fill{"."},
-        indicators::option::Lead{"<==>"},
-        indicators::option::End{"]"},
-        indicators::option::PrefixText{"Reading in SMT2"},
-        indicators::option::ForegroundColor{indicators::Color::yellow},
-        indicators::option::FontStyles{std::vector<indicators::FontStyle>{indicators::FontStyle::bold}}};
+    const auto num_chunks = (sz + chunk_sz_bytes - 1) / chunk_sz_bytes;
 
-    indicators::show_console_cursor(false);
+    vector_t<uint8_t *> thread_bufs;
+    for (size_t i = 0; i < 16; ++i) { // FIXME: HACK
+        uint8_t *buf = (uint8_t *)aligned_alloc(16 * 1024, buf_sz);
+        assert(buf);
+        thread_bufs.push_back(buf);
+    }
+
+    vector_t<uint8_t> non_zero_chunks(num_chunks);
+
+    std::atomic<size_t> num_nonzero_chunks{};
+    std::atomic<size_t> num_chunks_done{};
+
+    BlockProgressBar bar{// option::BarWidth{80},
+                         option::Start{"["}, option::End{"]"}, option::ForegroundColor{Color::yellow},
+                         option::FontStyles{std::vector<FontStyle>{FontStyle::bold}}};
+
+    // indicators::show_console_cursor(false);
 
     for (size_t i = 0; i < num_chunks; ++i) {
-        const size_t start_elem_off       = i * ((1024 * 1024 * 1024) / sizeof(uint64_t));
-        const size_t end_elem_off_full    = (i + 1) * ((1024 * 1024 * 1024) / sizeof(uint64_t));
-        const size_t end_elem_off_trimmed = std::min(end_elem_off_full, sz / sizeof(uint64_t));
-        const auto slice_start            = &mapping[start_elem_off];
-        const auto slice_end              = &mapping[end_elem_off_trimmed];
-        const auto slice_cnt_64           = end_elem_off_trimmed - start_elem_off;
-        pool.detach_task([slice_start, slice_cnt_64, &num_nonzero_gb, &bar] {
-            const auto hnz = has_nonzero(slice_start, slice_cnt_64);
-            if (hnz) {
-                ++num_nonzero_gb;
-            }
-            bar.tick();
-        });
+        const size_t start_elem_off    = i * (chunk_sz_bytes / sizeof(uint64_t));
+        const size_t end_elem_off_full = (i + 1) * (chunk_sz_bytes / sizeof(uint64_t));
+        const size_t end_elem_off      = std::min(end_elem_off_full, sz / sizeof(uint64_t));
+        const auto slice_cnt_64        = end_elem_off - start_elem_off;
+        if (!slice_cnt_64) {
+            continue;
+        }
+        // pool.detach_task([i, fd, buf_sz, start_elem_off, slice_cnt_64, &non_zero_chunks, &num_nonzero_chunks,
+        // &num_chunks_done, &bar, num_chunks, &thread_bufs] {
+        const auto hnz = has_nonzero(fd, buf_sz, start_elem_off, slice_cnt_64, thread_bufs);
+        if (hnz) {
+            num_nonzero_chunks.fetch_add(1, std::memory_order_relaxed);
+            non_zero_chunks[i] = 1;
+        }
+        const auto this_chunk = num_chunks_done.fetch_add(1, std::memory_order_relaxed);
+        bar.set_progress(double(this_chunk) / (100 * num_chunks));
+        fmt::print(" #: {} # tot: {} # non-0: {}", this_chunk, num_chunks,
+                   num_nonzero_chunks.load(std::memory_order_relaxed));
+        // usleep(10000 * this_chunk);
+        // });
     }
     pool.wait();
 
     bar.mark_as_completed();
-    indicators::show_console_cursor(true);
+    // indicators::show_console_cursor(true);
+
+    for (auto *buf : thread_bufs) { // FIXME: HACK
+        free(buf);
+    }
+
+    fmt::print("num_chunks: {} num_chunks_done: {}\n", num_chunks, num_chunks_done);
+    fmt::print("num_nonzero_chunks: {}\n", num_nonzero_chunks);
 }
 
 // using folly::small_vector_policy::policy_size_type;
@@ -259,35 +303,48 @@ void search_disk(const uint64_t *const mapping, size_t sz, std::atomic<size_t> &
 // folly::small_vector<int, 8, policy_size_type<uint16_t>> sv;
 
 int main(int argc, const char **argv) {
-    if (argc != 2) {
-        fmt::print(stderr, "usage: z3-bench <path to directory with .stm2 files>\n");
+    if (argc != 3) {
+        fmt::print(stderr, "usage: disk-test <path to block dev> <decimal block size in KiB>\n");
         return -1;
     }
     set_thread_priority_11();
     const auto disk_path = fs::path{argv[1]};
-    std::atomic<size_t> num_nonzero_gb{0};
-    std::mutex blobs_mutex;
+    const auto buf_sz_kb = atoi(argv[2]);
+    assert(buf_sz_kb > 0);
     const auto fd = ::open(disk_path.c_str(), O_RDONLY);
     assert(fd >= 0);
+
+    uint32_t block_sz{};
+    uint64_t block_count{};
+
     struct stat st;
     assert(!::fstat(fd, &st));
-    const auto sz = static_cast<size_t>(st.st_size);
+    size_t sz = static_cast<size_t>(st.st_size);
     if (!sz) {
-        assert(!::close(fd));
-        return 1;
+        if (ioctl(fd, DKIOCGETBLOCKSIZE, &block_sz) != 0) {
+            perror("DKIOCGETBLOCKSIZE");
+            ::close(fd);
+            return 1;
+        }
+
+        if (ioctl(fd, DKIOCGETBLOCKCOUNT, &block_count) == -1) {
+            ::perror("DKIOCGETBLOCKCOUNT");
+            ::close(fd);
+            return 1;
+        }
+        sz = block_sz * block_count;
+        if (!sz) {
+            fmt::print(::stderr, "sz is zero\n");
+            assert(!::close(fd));
+            return 1;
+        }
     }
-    fmt::print("{} sz: {}\n", disk_path, sz);
+
+    const auto buf_sz = buf_sz_kb * 1024;
+    fmt::print("{} fd: {} sz: {} buf_sz\n", disk_path, fd, sz, buf_sz);
     assert(sz % sizeof(uint64_t) == 0);
-    auto res                      = vector_t<char>(sz);
-    const uint64_t *const mapping = (uint64_t *const)::mmap(nullptr, sz, PROT_READ, MAP_PRIVATE, fd, 0);
-    assert(!::close(fd));
     BS::thread_pool tp;
-    const auto sz_kb      = sz / 1024;
-    const auto sz_mb      = sz_kb / 1024;
-    const auto sz_gb      = sz_mb / 1024;
-    const auto num_checks = sz_gb + 1;
-    vector_t<uint8_t> non_zero(sz_gb);
-    search_disk(mapping, sz, num_nonzero_gb, non_zero, blobs_mutex, tp);
-    fmt::print("num .smt2 files: {:d}\n", num_nonzero_gb.load());
+    search_disk(fd, sz, buf_sz, tp);
+    assert(!::close(fd));
     return 0;
 }
