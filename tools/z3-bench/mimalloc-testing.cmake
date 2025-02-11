b959a56fbbb470517f3fe25696bebf09f22fac45
Jevin Sweval <jevinsweval@gmail.com>
2025-02-10 20:34:53 -0500

    dirty tricks work, get smart kiddos

--- tools/z3-bench/mimalloc-testing.cmake
+++ tools/z3-bench/mimalloc-testing.cmake
@@ -5,7 +5,7 @@ if (CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
   set(CMAKE_LINK_LIBRARY_USING_UPWARD_LIBRARY_SUPPORTED TRUE)
 endif()
 
-add_library(mimalloc-tester-c SHARED mimalloc-tester-dylib-c.c noalloc-stdio.c malloc-wrapped.c mimalloc-fishhook.c fishhook.c mimalloc-visibility.c mimalloc-extern.c)
+add_library(mimalloc-tester-c SHARED mimalloc-tester-dylib-c.c noalloc-stdio.c malloc-wrapped.c mimalloc-fishhook.c fishhook.c mimalloc-extern.c)
 # target_link_libraries(mimalloc-tester-c PRIVATE mimalloc-obj mimalloc-interposing)
 target_link_libraries(mimalloc-tester-c PRIVATE mimalloc-obj)
 set_target_properties(mimalloc-tester-c PROPERTIES
@@ -55,10 +55,38 @@ set_target_properties(mimalloc-umbrella PROPERTIES
 add_executable(mimalloc-tester mimalloc-tester-exe.c noalloc-stdio.c)
 # target_link_libraries(mimalloc-tester PRIVATE mimalloc-tester-c)
 # target_link_libraries(mimalloc-tester PRIVATE mimalloc-tester-c-reexported)
-target_link_libraries(mimalloc-tester PRIVATE mimalloc-umbrella)
+# target_link_libraries(mimalloc-tester PRIVATE mimalloc-umbrella)
 # add_dependencies(mimalloc-tester mimalloc-tester-c)
 # target_link_options(mimalloc-tester PRIVATE "-Wl,-upward_library,$<TARGET_FILE:mimalloc-tester-c>,-rpath,$<PATH:GET_PARENT_PATH,$<TARGET_FILE:mimalloc-tester-c>>")
 set_target_properties(mimalloc-tester PROPERTIES
+    C_STANDARD 17
+    C_EXTENSIONS ON
+    C_STANDARD_REQUIRED ON
+    EXCLUDE_FROM_ALL ON
+)
+
+add_library(symbol-stubs SHARED symbol-stubs.c)
+# one weird trick 2 link mach-o dylibs against executables
+target_link_options(symbol-stubs PRIVATE "-Wl,-install_name,@rpath/mimalloc-tester-trick")
+set_target_properties(mimalloc-tester PROPERTIES
+    C_STANDARD 17
+    C_EXTENSIONS ON
+    C_STANDARD_REQUIRED ON
+    EXCLUDE_FROM_ALL ON
+)
+
+add_library(dyld-interposing SHARED dyld-interposing.c)
+target_link_libraries(dyld-interposing PRIVATE symbol-stubs)
+set_target_properties(dyld-interposing PROPERTIES
+    C_STANDARD 17
+    C_EXTENSIONS ON
+    C_STANDARD_REQUIRED ON
+    EXCLUDE_FROM_ALL ON
+)
+
+add_executable(mimalloc-tester-trick mimalloc-tester-exe.c noalloc-stdio.c mimalloc-tester-dylib-c.c noalloc-stdio.c malloc-wrapped.c mimalloc-extern.c)
+target_link_libraries(mimalloc-tester-trick PRIVATE mimalloc-obj dyld-interposing)
+set_target_properties(mimalloc-tester-trick PROPERTIES
     C_STANDARD 17
     C_EXTENSIONS ON
     C_STANDARD_REQUIRED ON
--- tools/z3-bench/mimalloc-visibility.c
+++
@@ -1,4 +0,0 @@
-#include "mimalloc-types.h"
-#include "misc-decls.h"
-
-extern SYM_EXT malloc_t mi_malloc_ext;
